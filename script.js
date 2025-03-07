fa337c58e3b3e6e9ffb2d6478766ab5c67ccf298
Jehangir Shah <53529731+jehangir1@users.noreply.github.com>
2025-03-07 23:33:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// 4. Removing the last element of an array using pop() and printing the removed element.
+let fruitsArray = ["Apple", "Banana", "Orange", "Pineapple", "Mango" ];
+let removedElement = fruitsArray.pop();
+alert(removedElement + " " + "is removed." + " " + "Please ceck the console log."); // Mango
+console.log(fruitsArray);
